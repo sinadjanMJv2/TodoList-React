74a65533597aa535a4c3d8aad2bea567d49ff933
sinadjanMJv2 <sinadjanmichaeljayv2@gmail.com>
2024-09-12 20:09:39 +0800

    add words identifier

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,11 @@ import ComponentApp from './ComponentApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1>This Method did not use Component </h1>
     <App />
+    <br /><br /><br />
+    <hr /><br /><br /><br />
+    <h1>This Method use Component </h1>
     <ComponentApp/>
   
   </React.StrictMode>,
